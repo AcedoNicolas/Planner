75c32258a66299eb2f0878ff42bdb60df8a087f9
AcedoNicolas <acedo_nicolas@hotmail.com>
2018-08-20 18:40:26 +0200

    initial commit from previous version

---
+++ Authentication/forgot.php
@@ -0,0 +1,58 @@
+<?php 
+/* Reset your password form, sends reset.php password link */
+require 'db.php';
+session_start();
+
+// Check if form submitted with method="post"
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) 
+{   
+    $email = $mysqli->escape_string($_POST['email']);
+    $result = $mysqli->query("SELECT * FROM users WHERE email='$email'");
+
+    if ( $result->num_rows == 0 ) // User doesn't exist
+    { 
+        $_SESSION['message'] = "User with that email doesn't exist!";
+        header("location: error.php");
+    }
+    else { // User exists (num_rows != 0)
+
+        $user = $result->fetch_assoc(); // $user becomes array with user data
+        
+        $email = $user['email'];
+        $hash = $user['hash'];
+        $first_name = $user['first_name'];
+
+
+        header("location: reset.php");
+  }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Reset Your Password</title>
+  <?php include 'css/css.html'; ?>
+</head>
+
+<body>
+    
+  <div class="form">
+
+    <h1>Reset Your Password</h1>
+
+    <form action="forgot.php" method="post">
+     <div class="field-wrap">
+      <label>
+        Email Address<span class="req">*</span>
+      </label>
+      <input type="email"required autocomplete="off" name="email"/>
+    </div>
+    <button class="button button-block"/>Reset</button>
+    </form>
+  </div>
+          
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="../js/index.js"></script>
+</body>
+
+</html>
---
+++ Tasks/add/add.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Planner</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<body>
+
+<div class="page">
+    <div class="pageHeader">
+        <div class="title">My Planner</div>
+        <div class="userPanel">
+            <i class="fa fa-chevron-down"></i>
+            <span class="username">Acedo Nicolas </span>
+            <img src=Profile.png width="40" height="40"/>
+        </div>
+    </div>
+    <div class="main">
+        <div class="nav">
+            <div class="searchbox">
+                <div><i class="fa fa-search"></i>
+                    <input type="search" placeholder="Search"/>
+                </div>
+            </div>
+            <div class="menu">
+                <div class="title">Navigation</div>
+                <ul>
+                    <li><i class="fa fa-home"></i>Home</li>
+                    <li class="active"><i class="fa fa-tasks"></i>Manage Tasks</li>
+                </ul>
+            </div>
+        </div>
+        <div class="view">
+            <div class="viewHeader">
+                <div class="title">Manage Tasks</div>
+                <div class="functions">
+                    <div class="button active">Add New Task</div>
+                    <div class="button">Add New List</div>
+                    <div class="button inverz"><i class="fa fa-trash-o"></i></div>
+                </div>
+            </div>
+            <div class="content">
+                <div class="list">
+                    <div class="title">School Stuff</div>
+                    <ul>
+                        <li class="checked"><i class="fa fa-check-square-o"></i><span>Php exam</span>
+                            <div class="info">
+                                <div class="button green">In progress</div>
+                                <span>Complete by 29/08/2018</span>
+                            </div>
+                        </li>
+                        <li><i class="fa fa-square-o"></i><span>Design a new logo</span>
+                            <div class="info">
+                                <div class="button">Pending</div>
+                                <span>Complete by 15/09/2019</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="list">
+                    <div class="title">Daily stuff</div>
+                    <ul>
+                        <li>
+                            <i class="fa fa-square-o"></i><span>Buy shampoo for mom</span>
+                            <div class="info">
+                                <div class="button">Done</div>
+                                <span>Completed by 31/07/2019</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="../js/index.js"></script>
+</body>
+</html>
