5f3266cd95b1f28379554ea21e93910f4d1cff9b
AcedoNicolas <acedo_nicolas@hotmail.com>
2018-08-28 22:13:18 +0200

    added toggle for todo and doing

---
+++ overview/update-task-status.php
@@ -0,0 +1,16 @@
+<?php
+
+include '../config/session.php';
+include '../config/database_connection.php';
+
+// https://stackoverflow.com/questions/4064444/returning-json-from-a-php-script
+$var = $_GET['taskListId'];
+if ($var === 0) {
+    $fetch_tasks = $mysqli->query("SELECT * FROM tasks WHERE userid='$user_id'");
+} else {
+    $fetch_tasks = $mysqli->query("SELECT * FROM tasks WHERE userid='$user_id' AND tasklistid='$var'");
+}
+
+$data = mysqli_fetch_all($fetch_tasks, MYSQLI_ASSOC);
+header('Content-Type: application/json'); //JSON object maken om in JS makkelijk te kunnen loopen door data
+echo json_encode($data); //JSON object maken om in JS makkelijk te kunnen loopen door data
